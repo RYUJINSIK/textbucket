0e6e5580c92b8c3dbd2d3cd3390fe4958f15522c
RYUJINSIK <64420030+RYUJINSIK@users.noreply.github.com>
2024-03-20 17:38:54 +0900

    [#50] : 마이페이지 수정하기 기능구현 (#51)

--- app/my/edit/page.tsx
+++ app/my/edit/page.tsx
@@ -5,7 +5,7 @@ import { useAuth } from "@/shared/contexts/AuthContext";
 import { useForm } from "react-hook-form";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent, FormEvent } from "react";
 import axios from "axios";
 import Modal from "@/components/Modal";
 
@@ -23,23 +23,75 @@ const ProfileEditPage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const {
     register,
-    reset,
-    handleSubmit,
     formState: { isValid, isDirty },
   } = useForm<IProfileFormProps>({
     mode: "onChange",
   });
 
-  useEffect(() => {
-    reset({
-      imageUrl: profile?.imageUrl,
-      nickName: profile?.nickName,
-      description: profile?.description,
-    });
-  }, []);
+  const [isError, setIsError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // 닉네임 정규식
+  const regex = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/;
+  const [previewURL, setPreviewURL] = useState<any>(null);
+  const [formData, setFormData] = useState({
+    imageUrl: profile?.imageUrl as string,
+    nickName: profile?.nickName as string,
+    description: profile?.description as string,
+    file: null as any,
+  });
+
+  const { imageUrl, nickName, description, file } = formData;
+
+  const handleNickNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, nickName: e.target.value });
+  };
+  const handleDescriptionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, description: e.target.value });
+  };
+
+  const handleImageChange = (event: { target: { files?: any } }) => {
+    setFormData({ ...formData, file: event.target.files[0] });
+    console.log("???? : ", event.target.files[0]);
+    setPreviewURL(URL.createObjectURL(event.target.files[0]));
+    closeModal();
+  };
+
+  const getImageUrl = async () => {
+    const imageData = new FormData();
+    imageData.append("files", formData.file);
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/image`,
+        imageData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          maxBodyLength: Infinity,
+          maxContentLength: Infinity,
+        }
+      );
+      onSubmit(response.data[0].imageUrl);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const onClickLogout = () => {
     setIsOpen(true);
+  };
+
+  const defaultImage = () => {
+    setPreviewURL("");
+    closeModal();
   };
 
   const Logout = () => {
@@ -51,33 +103,42 @@ const ProfileEditPage = () => {
     setIsSigned(false);
     router.push("/");
   };
-  const onSubmit = async (data: IProfileFormProps) => {
-    const res = await axios.put(
-      `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/member`,
-      data,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    if (res.status === 200 && profile) {
-      setProfile({
-        ...profile,
-        imageUrl: data.imageUrl,
-        nickName: data.nickName,
-        description: data.description,
-      });
-      localStorage.setItem(
-        "profile",
-        JSON.stringify({
-          ...profile,
-          imageUrl: data.imageUrl,
-          nickName: data.nickName,
-          description: data.description,
-        })
+  const onSubmit = async (imageUrl: string) => {
+    const requestBody = {
+      imageUrl: imageUrl,
+      nickName: nickName,
+      description: description,
+    };
+    if (regex.test(nickName)) {
+      const res = await axios.put(
+        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/member`,
+        requestBody,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
       );
-      alert("수정 성공");
+      if (res.status === 200 && profile) {
+        setProfile({
+          ...profile,
+          imageUrl: imageUrl,
+          nickName: nickName,
+          description: description,
+        });
+        localStorage.setItem(
+          "profile",
+          JSON.stringify({
+            ...profile,
+            imageUrl: imageUrl,
+            nickName: nickName,
+            description: description,
+          })
+        );
+        router.push("/my");
+      }
+    } else {
+      setIsError(true);
     }
   };
   return (
@@ -93,17 +154,24 @@ const ProfileEditPage = () => {
             <div className="flex-grow-0 flex-shrink-0 w-[88px] h-[88px] relative rounded-full bg-[#ededed]">
               <img
                 className="w-full h-full rounded-full overflow-hidden"
-                src={profile?.imageUrl}
+                src={
+                  previewURL === ""
+                    ? "/icons/profile_default.png"
+                    : previewURL
+                    ? previewURL
+                    : profile?.imageUrl === ""
+                    ? "/icons/profile_default.png"
+                    : profile?.imageUrl
+                }
                 alt="profile"
               />
-
               <Image
                 src="/icons/camera.png"
                 width={24}
                 height={24}
                 alt="이미지삭제"
                 onClick={() => {
-                  alert("zz");
+                  openModal();
                 }}
                 className="w-6 h-6 absolute left-16 top-16"
               />
@@ -115,23 +183,34 @@ const ProfileEditPage = () => {
               닉네임
             </p>
             <p className="mt-5 mb-2 font-medium text-[#777777] text-sm">
-              {profile?.nickName.length} / 10
+              {nickName.length} / 10
             </p>
           </div>
           <input
             type="text"
-            id="nickName"
-            {...register("nickName")}
+            name="nickName"
+            value={nickName}
+            onChange={handleNickNameChange}
             maxLength={10}
-            className="border-[#e3e3e3] p-3 rounded-lg border w-full resize-none text-center text-base text-[#353535] h-[50px]"
+            className={`${
+              isError ? "border-[#eb3434]" : "border-[#e3e3e3]"
+            } p-3 rounded-lg border w-full resize-none text-center text-base text-[#353535] h-[50px]`}
           />
+
+          <p className="mt-1 mb-2 font-medium text-[#eb3434] text-xs h-1">
+            {isError
+              ? "닉네임은 띄어쓰기 없이 한글, 영문, 숫자만 가능해요."
+              : ""}
+          </p>
           <p className="mt-5 mb-2 font-medium text-[#131313] text-sm">
             소개 한 마디
           </p>
           <textarea
             id="description"
-            {...register("description")}
             maxLength={100}
+            name="description"
+            value={description}
+            onChange={handleDescriptionChange}
             placeholder="소개 한 마디를 써 보세요:)"
             className="border-[#e3e3e3] p-3 rounded-lg border w-full resize-none text-xs text-[#353535] h-[100px]"
           />
@@ -139,7 +218,9 @@ const ProfileEditPage = () => {
         <div className="flex items-end justify-end">
           <button
             type="submit"
-            onClick={handleSubmit(onSubmit)}
+            onClick={() => {
+              file !== null ? getImageUrl() : onSubmit("");
+            }}
             className="mt-5 mb-5 w-full py-4 rounded-lg text-white text-center bg-[#00C37D] text-sm font-bold"
           >
             적용하기
@@ -157,6 +238,38 @@ const ProfileEditPage = () => {
             <span className="text-[#999] font-bold">로그아웃</span>
           </div> */}
         </div>
+
+        {isModalOpen && (
+          <>
+            <div
+              className="fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-[60]"
+              onClick={closeModal}
+            />
+            <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-white z-[70] w-full max-w-[390px] rounded-t-xl">
+              <label htmlFor="imageUpload">
+                <div className="flex flex-col items-center justify-center py-4 cursor-pointer	">
+                  <h3>이미지 변경</h3>
+                </div>
+                <input
+                  type="file"
+                  id="imageUpload"
+                  className="hidden" // 파일 입력 요소를 숨깁니다.
+                  name="filename"
+                  accept="image/*"
+                  onChange={handleImageChange} // 파일이 변경되었을 때 호출될 핸들러 함수
+                />
+              </label>
+
+              <hr />
+              <div
+                className="flex flex-col items-center justify-center py-4 cursor-pointer"
+                onClick={defaultImage}
+              >
+                <h3>기본 이미지로 변경</h3>
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <Modal
         open={isOpen}

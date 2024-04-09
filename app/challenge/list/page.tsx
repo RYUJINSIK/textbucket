3162df2b6167d9a818e583b5ea6b18ee0a004ca0
RYUJINSIK <it_jinsik@naver.com>
2024-04-10 00:18:23 +0900

    챌린지리스트 페이지 추가

---
+++ app/challenge/list/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useState, useEffect, ChangeEvent } from "react";
+import WithHeaderLayout from "@/components/WithHeaderLayout";
+const ChallengeListPage: React.FC = () => {
+  return (
+    <WithHeaderLayout>
+      <div>ChallengeListPage</div>
+    </WithHeaderLayout>
+  );
+};
+export default ChallengeListPage;

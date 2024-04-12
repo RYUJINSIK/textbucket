4f8e8acbb2c08548f4a9276e68c674742cffab60
RYUJINSIK <it_jinsik@naver.com>
2024-04-12 16:55:37 +0900

    챌린지 리스트 페이지 api 연결

--- app/challenge/list/page.tsx
+++ app/challenge/list/page.tsx
@@ -1,7 +1,32 @@
 "use client";
 import { useState, useEffect, ChangeEvent } from "react";
+import axios from "axios";
 import WithHeaderLayout from "@/components/WithHeaderLayout";
 const ChallengeListPage: React.FC = () => {
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const accessToken =
+    typeof window !== "undefined" && localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/challenge/list?page=${page}&size=${pageSize}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log("!", error);
+      });
+  }, []);
+
   return (
     <WithHeaderLayout>
       <div>ChallengeListPage</div>

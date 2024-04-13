7c7516423468979016c1e98a6496c190c9f4783b
RYUJINSIK <it_jinsik@naver.com>
2024-04-13 15:01:13 +0900

    챌린지 메인페이지 ui

--- app/challenge/page.tsx
+++ app/challenge/page.tsx
@@ -1,29 +1,50 @@
 "use client";
+import { useState, useEffect, ChangeEvent } from "react";
+import axios from "axios";
+import Link from "next/link";
 import WithHeaderLayout from "@/components/WithHeaderLayout";
-
 const ChallengePage = () => {
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const accessToken =
+    typeof window !== "undefined" && localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/challenge/list?page=${page}&size=${pageSize}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log("!", error);
+      });
+  }, []);
   return (
     <WithHeaderLayout>
-      <div className="grid place-items-center h-full">
-        <div className="flex flex-col justify-center items-center w-[328px] h-[494px] relative gap-2.5 p-4 rounded-xl bg-[#e6fff6]">
-          <p className="flex-grow-0 flex-shrink-0 text-lg font-semibold text-center text-black">
-            <span className="flex-grow-0 flex-shrink-0 text-lg font-semibold text-center text-black">
-              {" "}
-              📅{" "}
-            </span>
-            <br />
-            <span className="flex-grow-0 flex-shrink-0 text-lg font-semibold text-center text-black">
-              챌린지 기간을 설정하고{" "}
-            </span>
-            <br />
-            <span className="flex-grow-0 flex-shrink-0 text-lg font-semibold text-center text-black">
+      <div className="h-screen flex justify-center items-center bg-[#E6FFF6] m-3 rounded-xl">
+        <div className="flex flex-col gap-y-4 w-full max-w-[500px] items-center justify-center">
+          <div className="flex flex-col gap-y-8 items-center justify-center">
+            <p className="text-center font-semibold text-lg text-[#353535]">
+              📅
+              <br />
+              챌린지 기간을 설정하고
+              <br />
               기간동안 필사를 진행해 보세요 :)
-            </span>
-          </p>
-          <div className="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 h-[51px] relative gap-1 px-6 py-4 rounded-xl bg-[#00c37d]">
-            <p className="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">
-              챌린지 만들기
             </p>
+            <Link
+              href={"/challenge/create"}
+              className="w-full py-4 rounded-lg text-white text-center bg-[#00C37D] text-lg font-bold"
+            >
+              + 챌린지 만들기
+            </Link>
           </div>
         </div>
       </div>

f68a95aebc55e51862f8422f6bce7c0c3cb74f7e
RYUJINSIK <it_jinsik@naver.com>
2024-04-24 23:15:14 +0900

    챌린지 화면 변경

--- app/challenge/page.tsx
+++ app/challenge/page.tsx
@@ -3,13 +3,31 @@ import { useState, useEffect, ChangeEvent } from "react";
 import axios from "axios";
 import Link from "next/link";
 import WithHeaderLayout from "@/components/WithHeaderLayout";
+
+export interface IChallengeItem {
+  id: number;
+  startDate: string;
+  endDate: string;
+  achievementRate: number;
+  title: string;
+  description: string;
+}
+interface ICategoryItem {
+  categoryCd: number;
+  categoryName: string;
+  description: string;
+}
 const ChallengePage = () => {
   const [page, setPage] = useState(0);
   const [pageSize, setPageSize] = useState(10);
   const [status, setStatus] = useState(["ING"]);
   const accessToken =
     typeof window !== "undefined" && localStorage.getItem("accessToken");
-
+  const [exist, setExist] = useState(false);
+  const [challenge, setChallenge] = useState<IChallengeItem | undefined>(
+    undefined
+  );
+  const [categoryList, setCategoryList] = useState<any>([]);
   useEffect(() => {
     axios
       .get(
@@ -22,8 +40,10 @@ const ChallengePage = () => {
         }
       )
       .then((res) => {
-        if (res.data.totalCount >= 1)
+        if (res.data.totalCount >= 1) {
+          setExist(true);
           getChallengeDetail(res.data.challengeInfos[0].id);
+        }
       })
       .catch((error) => {
         console.log("!", error);
@@ -44,6 +64,8 @@ const ChallengePage = () => {
       .then((res) => {
         console.log("??");
         console.log(res.data);
+        setChallenge(res.data);
+        setCategoryList(res.data.categoryListDto);
       })
       .catch((error) => {
         console.log("!", error);
@@ -51,25 +73,43 @@ const ChallengePage = () => {
   };
   return (
     <WithHeaderLayout>
-      <div className="h-screen flex justify-center items-center bg-[#E6FFF6] m-3 rounded-xl">
-        <div className="flex flex-col gap-y-4 w-full max-w-[500px] items-center justify-center">
-          <div className="flex flex-col gap-y-8 items-center justify-center">
-            <p className="text-center font-semibold text-lg text-[#353535]">
-              📅
-              <br />
-              챌린지 기간을 설정하고
-              <br />
-              기간동안 필사를 진행해 보세요 :)
-            </p>
-            <Link
-              href={"/challenge/create"}
-              className="w-full py-4 rounded-lg text-white text-center bg-[#00C37D] text-lg font-bold"
-            >
-              + 챌린지 만들기
-            </Link>
+      {exist ? (
+        challenge && (
+          <>
+            <p>data.title : {challenge.title}</p>
+            <p>data.startDate : {challenge.startDate}</p>
+            <p>data.endDate : {challenge.endDate}</p>
+            <p>data.description : {challenge.description}</p>
+
+            {Object.entries<ICategoryItem>(categoryList).map(
+              ([key, category]) => (
+                <p key={key}>data.categoryListDto : {category.categoryName}</p>
+              )
+            )}
+            <p>data.achievementRate : {challenge.achievementRate}</p>
+          </>
+        )
+      ) : (
+        <div className="h-screen flex justify-center items-center bg-[#E6FFF6] m-3 rounded-xl">
+          <div className="flex flex-col gap-y-4 w-full max-w-[500px] items-center justify-center">
+            <div className="flex flex-col gap-y-8 items-center justify-center">
+              <p className="text-center font-semibold text-lg text-[#353535]">
+                📅
+                <br />
+                챌린지 기간을 설정하고
+                <br />
+                기간동안 필사를 진행해 보세요 :)
+              </p>
+              <Link
+                href={"/challenge/create"}
+                className="w-full py-4 rounded-lg text-white text-center bg-[#00C37D] text-lg font-bold"
+              >
+                + 챌린지 만들기
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </WithHeaderLayout>
   );
 };
